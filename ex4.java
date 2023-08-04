38dc342acc4769f85dd1bc389a0f990e48c2c25e
rahulsenthil90 <rahulsenthil90@gmail.com>
2023-08-04 13:27:50 +0530

    added ex4

---
+++ ex4.java
@@ -0,0 +1,26 @@
+package JavaCoding;
+
+public class ex4 {
+	public static void main(String[] args) {
+		String s1="abbcdd";
+		char a[]=s1.toCharArray();
+		int n=2,i=1;
+		for(int j=0;j<a.length-1;j++){
+		    int k=j+1;
+		    if(a[k]==a[j]){
+		        i++;
+		    }
+		    if(a[k]!=a[j]){
+		        i=1;
+		    }
+		    if(i==n){
+		        while(i!=0){
+		            a[k--]=0;
+		            i--;
+		        }
+		        i=1;
+		    }
+		}
+		System.out.print(a);
+	}
+}
