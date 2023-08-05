6c6cf5764efd75a9c564f0b3358bfd70a101a3b5
rahulsenthil90 <rahulsenthil90@gmail.com>
2023-08-05 09:56:01 +0530

    Modified ex7

--- ex7.java
+++ ex7.java
@@ -4,8 +4,8 @@ public class ex7 {
     public static void main(String[] args) {
 		int a[]={4,6,8,3,9,-1,5,2};
 		int i=0,j=0;
-		for(i=0;i<8;i++){
-		   for(j=0;j<7;j++){
+		for(i=0;i<8-1;i++){
+		   for(j=0;j<8-1-i;j++){
 		       if(a[j]>a[j+1]){
 		           int t=a[j];
 		           a[j]=a[j+1];
