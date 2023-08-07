d29cbaa1f97651ca9174b5a3b8c73cc672206af4
rahulsenthil90 <rahulsenthil90@gmail.com>
2023-08-07 14:12:26 +0530

    added ex10

---
+++ ex10.java
@@ -0,0 +1,15 @@
+package JavaCoding;
+
+public class ex10 {
+    public static void main(String[] args) {
+		int arr[]={4,2,6,8,3,2};
+		PrintArray(arr,0);
+	}
+	public static void PrintArray(int A[],int i){
+	    if(i==A.length){
+	        return;
+	    }
+	    System.out.print(A[i]+" ");
+	    PrintArray(A,i+1);
+	}
+}
